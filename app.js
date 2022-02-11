3ff1d0631d6456a14a3dffcfe4f07b5d29dfbf64
Jose Marcelino Catacutan <jomarcatacutan@Joses-MacBook-Pro.local>
2022-02-11 14:37:22 +0800

    done with 1-6

---
+++ app.js
@@ -0,0 +1,29 @@
+// [SECTION] Dependencies and Modules
+	const express = require("express");
+	const mongoose = require("mongoose");
+	const dotenv = require("dotenv");
+	const cors = require("cors");
+
+// [SECTION] Environment Variables Setup
+	dotenv.config();
+	const port = process.env.PORT;
+	const credentials = process.env.MONGO_ACCESS;
+
+// [SECTION] Server Setup
+	const app = express();
+	app.use(express.json());
+
+// [SECTION] Database Connect
+	mongoose.connect(credentials);
+	const db = mongoose.connection;
+	db.once("open", () => console.log("Connected to MongoDB Atlas"));
+
+// [SECTION] Server Routes
+
+// [SECTION] Server Responses
+	app.get("/", (req, res) => {
+		res.send("This is my Capstone 2 Project!");
+	});
+	app.listen(port, () => {
+		console.log(`API is online on port ${port}`);
+	});
